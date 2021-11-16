f2edf0ffed06818349e8567063397c5b7b0b2e2f
Sarfaraz Alam <71469354+sarfarazstark@users.noreply.github.com>
2021-11-17 04:35:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ DEFAULT_SEARCH_MARKUP = [
                      InlineKeyboardButton("Go Inline", switch_inline_query="")],
                     [InlineKeyboardButton("Search Anime", switch_inline_query_current_chat="!a "),
                      InlineKeyboardButton("GO Inline", switch_inline_query_current_chat="!a ")],
-                    [InlineKeyboardButton("Developer: @SarfarazStark", url="https://telegram.me/SarfarazStark"]
+                    [InlineKeyboardButton("Developer: @SarfarazStark", url="https://telegram.me/SarfarazStark")]
                 ]
 
 
